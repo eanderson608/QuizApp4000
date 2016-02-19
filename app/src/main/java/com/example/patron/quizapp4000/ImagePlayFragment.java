5a9a3ee23a37029ebcc683fbbc3137151d9d3779
Eric Anderson <eanderson608@gmail.com>
2016-02-18 21:07:05 -0600

    added picture of dog

--- app/src/main/java/com/example/patron/quizapp4000/ImagePlayFragment.java
+++ app/src/main/java/com/example/patron/quizapp4000/ImagePlayFragment.java
@@ -63,7 +63,7 @@ public class ImagePlayFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_play, container, false);
+        return inflater.inflate(R.layout.fragment_image_play, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
